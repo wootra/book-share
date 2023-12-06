4ba2396ff3240d7a25d611d4464344f1bdafb242
SONGHYEON JUN <shjeon0730@gmail.com>
2023-12-06 16:59:10 -0600

    client init modules, post resource test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css';
+import '~slick-carousel/slick/slick.css';
+import '~slick-carousel/slick/slick-theme.css';
+import './index.css';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,15 +1,27 @@
 # from server.config import db
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 # from flask_restful import reqparse, abort, Api, Resource
 from server.models.users import User
 from server.models.books import Book
 from server.models.rents import Rent
+
+parser = reqparse.RequestParser()
 
 
 class Users(Resource):
     def get(self):
         users = [user.to_dict() for user in User.query.all()]
         return users
+    
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('a', type=int, help='Rate to charge for a resource')
+        parser.add_argument('b', type=int, help='Rate to charge for b resource')
+        parser.add_argument('c', type=int, help='Rate to charge for c resource')
+        parser.add_argument('d', type=int, help='Rate to charge for d resource')
+        params = parser.parse_args()
+        print(params)
+        return params
 
 
 class Books(Resource):
