472d566d13fe80ce38b9d399f77024e9d284f0b5
SONGHYEON JUN <shjeon0730@gmail.com>
2023-12-07 22:49:52 -0600

    add nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,13 +1,17 @@
 import { useLocation, useNavigate } from 'react-router-dom';
-import { Menu } from 'semantic-ui-react';
+import { Dropdown, Image, Input, Menu } from 'semantic-ui-react';
 import styles from './Nav.module.css';
 const Nav = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const pathname = location.pathname;
+
     const onNavClick = (e, { name }) => navigate(name);
+    const onUserInfoClick = (e, { value }) => navigate(value);
+    const loginLabel = 'Songhyeon Jun';
     return (
-        <Menu className={styles.nav}>
+        <Menu className={styles.nav} pointing secondary>
+            <Image src='https://react.semantic-ui.com/logo.png' size='mini' />
             <Menu.Item
                 name='/home'
                 active={pathname === '/home'}
@@ -31,7 +35,29 @@ const Nav = () => {
             >
                 Lend
             </Menu.Item>
+
             <Menu.Menu position='right'>
+                <Menu.Item>
+                    <Input
+                        transparent
+                        icon={{ name: 'search', link: true }}
+                        placeholder='Search users...'
+                    />
+                </Menu.Item>
+                <Dropdown item icon='setting' text={loginLabel}>
+                    <Dropdown.Menu className={styles.userInfo}>
+                        <Dropdown.Item
+                            value='/userinfo/profile'
+                            content='/userinfo/profile'
+                            onClick={onUserInfoClick}
+                        >
+                            Profile
+                        </Dropdown.Item>
+
+                        <Dropdown.Divider />
+                        <Dropdown.Item>Log Out</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
                 <Menu.Item
                     name='/log-in'
                     active={pathname === '/log-in'}
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    css: {
+        devSourcemap: true,
+        modules: {
+            localsConvention: 'camelCaseOnly',
+        },
+    },
     plugins: [react()],
 });
