52dd41ada746afb821db2c7ae886fa321c395585
SONGHYEON JUN <shjeon0730@gmail.com>
2023-12-07 19:12:27 -0600

    update db path

--- server/api/index.py
+++ server/api/index.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask_migrate import Migrate
 from server.config import db
+import server.env_vars as env_vars
 from flask_restful import Api
 from server.routes import Users, Books, Rents
 
 app = Flask(__name__)
 migrate = Migrate(app, db)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(env_vars)
 
 db.init_app(app)
 api = Api(app)
